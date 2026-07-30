61ceef1bb5d7431993615d22b50dd63c51fa0296
techflashYT <officialTechflashYT@gmail.com>
2026-07-29 22:56:34 -0500

    gettimeofday is stubbed, use hw timers

--- main/port-gba.c
+++ main/port-gba.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 #include <string.h>
-#include <sys/time.h>
+#include <gba_timers.h>
 
 #include "../../host.h"
 
 extern struct MiniRV32IMAState core;
 extern void DumpState(struct MiniRV32IMAState *core);
 
-size_t GetTimeMicroseconds()
+#define TIMER_DIV_64	1
+
+static u32 lastTimerTicks;
+static uint64_t timerEpoch;
+
+uint64_t GetTimeMicroseconds()
 {
-	struct timeval tv;
-	gettimeofday(&tv, 0);
-	return tv.tv_usec + ((size_t)(tv.tv_sec)) * 1000000LL;
+	u16 highBefore, highAfter, low;
+	u32 ticks;
+	uint64_t extendedTicks;
+
+	/*
+	 * Timer 0 runs at 16.777216 MHz / 64 = 262144 Hz and timer 1
+	 * supplies its upper 16 bits.  Re-read around a timer 0 rollover
+	 * so the combined value cannot tear.
+	 */
+	do {
+		highBefore = REG_TM1CNT_L;
+		low = REG_TM0CNT_L;
+		highAfter = REG_TM1CNT_L;
+	} while (highBefore != highAfter);
+
+	ticks = ((u32)highAfter << 16) | low;
+	if (ticks < lastTimerTicks)
+		timerEpoch += UINT64_C(1) << 32;
+	lastTimerTicks = ticks;
+	extendedTicks = timerEpoch | ticks;
+
+	/* 1000000 / 262144 = 15625 / 4096 exactly */
+	return (extendedTicks * UINT64_C(15625)) >> 12;
 }
 
 int ReadKBByte(void)
@@ -28,7 +53,15 @@ int IsKBHit(void)
 
 int psram_init(void)
 {
-	/* nothing to initialize on GBA w/ link cable */
+	/* set up timers to emulate CLINT mtime counter */
+	REG_TM0CNT_H = 0;
+	REG_TM1CNT_H = 0;
+	REG_TM0CNT_L = 0;
+	REG_TM1CNT_L = 0;
+	lastTimerTicks = timerEpoch = 0;
+	REG_TM1CNT_H = TIMER_COUNT | TIMER_START;
+	REG_TM0CNT_H = TIMER_DIV_64 | TIMER_START;
+
 	return 0;
 }
 
